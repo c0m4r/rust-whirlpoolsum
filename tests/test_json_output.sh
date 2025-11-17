372249e48c34816a3b96741a9c511d70335c6826
c0m4r <github@wolfet.pl>
2025-11-17 22:02:41 +0100

    tests: json and yaml tests

---
+++ tests/test_json_output.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -e
+
+BASEDIR=$(dirname "$0")
+source "${BASEDIR}"/functions.sh
+
+trap 'rm -f "$TMPFILE"' EXIT
+
+TMPFILE=$(mktemp)
+
+echo "You may say I'm a dreamer But I'm not the only one" > "$TMPFILE"
+
+CHECKSUM=$("$CMD" "$TMPFILE")
+
+if [ "$CHECKSUM" == "1ae8fde50fcf4a117f0207164be4f95ffc004fec335e557f7492e3844d520b5b136aa9eacf4fa1cd6559196eb1182d98db04f92727bb1489ed0d0e49452b5149  $TMPFILE" ] ; then
+    "$CMD" --json "$TMPFILE" 2>&1 | jq -e .hash_results > /dev/null && pass PASS || fail FAIL
+else
+    fail "FAILED: wrong checksum"
+    exit 1
+fi
