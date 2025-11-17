2f08e08203406e6ee539be2461030f78cc0b581d
c0m4r <github@wolfet.pl>
2025-11-17 21:10:35 +0100

    build.sh: build both debug and release

--- build.sh
+++ build.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
-RUSTFLAGS="-C target-cpu=native -C link-arg=-Wl,-z,relro,-z,now" cargo build --release
+
+export RUSTFLAGS="-C target-cpu=native -C link-arg=-Wl,-z,relro,-z,now"
+cargo build
+cargo build --release
