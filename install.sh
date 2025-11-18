81dc9267eb60042dd9e76f12f32e98f79096f5ec
c0m4r <github@wolfet.pl>
2025-11-18 01:12:26 +0100

    install.sh: sudo/doas detection

--- install.sh
+++ install.sh
@@ -1,5 +1,17 @@
-#!/bin/sh
+#!/bin/bash
+
+SUDO=""
+
+if [ $(whoami) != "root" ]; then
+    if command -v sudo >/dev/null 2>&1 ; then
+        SUDO="sudo"
+    elif command -v doas >/dev/null 2>&1 ; then
+        SUDO="doas"
+    else
+        echo "no sudo/doas found, make sure you run that script as root"
+    fi
+fi
 
 echo "Installing to /usr/local/bin/whirlpoolsum"
-cp -v target/release/whirlpoolsum /usr/local/bin/
-ln -s whirlpoolsum /usr/local/bin/wrl512sum
+$SUDO cp -v target/release/whirlpoolsum /usr/local/bin/
+$SUDO ln -sfv whirlpoolsum /usr/local/bin/wrl512sum
