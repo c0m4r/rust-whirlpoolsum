6f2db32f59ebde95364e49c34ad2dc03cca0430c
c0m4r <github@wolfet.pl>
2025-11-21 00:57:03 +0100

    refine test wrapper

--- test.sh
+++ test.sh
@@ -2,8 +2,19 @@
 
 set -e
 
+BASEDIR=$(dirname "$0")
+
+source "${BASEDIR}/tests/functions.sh"
+
+if ! command -v unshare &>/dev/null 2>&1 ; then
+    echo "FAILED: you have to install unshare for network sandboxing"
+    exit 1
+fi
+
 if [ -d tests ]; then
-    find tests/ -maxdepth 1 -name 'test*.sh' -type f -exec bash {} \;
+    find ${BASEDIR}/tests/ -maxdepth 1 -name 'test*.sh' -type f | while read -r test ; do
+        unshare --net --map-root-user bash "$test" || fail FAILED
+    done
 else
     echo "FAILED: tests not found"
     exit 1
